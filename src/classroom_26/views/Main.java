58792c388b2c31fe34bb3831527a67b00996114b
cezarj59 <cezarj59@yahoo.com>
2022-12-15 20:23:45 -0300

    inicio

--- src/classroom_26/views/Main.java
+++ src/classroom_26/views/Main.java
@@ -1,12 +1,10 @@
 package classroom_26.views;
 
-import classroom_26.services.Receber;
 
 public class Main {
 
     public static void main(String[] args) {
-       // Menu.inicio();
-        Receber.data();
+        Menu.inicio();
     }
 
 }
